f85d1c9f154ce3552c1f757c7256cf8b8b0885df
sivag123 <sivag123@gmail.com>
2015-08-15 21:50:45 +0530

    SignUp module

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,9 +1,13 @@
 class UserController < ApplicationController
 	
 	def new 
-		@user = User.new
+		
 	end
 
 	def signup
+		@user = User.new(name: params[:name], mob_num: params[:mob_num], preferred_jobs: params[:preferred_jobs].join('/'), preferred_location: params[:preferred_location].join('/'),ratings: 0.0,available: true, experience: 0,password: params[:password])		
+		if @user.save
+			puts "Success"
+		end
 	end
 end
