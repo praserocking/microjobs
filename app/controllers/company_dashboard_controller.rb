9c7f2f64d3b8fe2ccbdf4574aac00cc2c086cdd1
sivag123 <sivag123@gmail.com>
2015-08-16 05:18:01 +0530

    Fixed Jobpost to Candidates Flow

--- app/controllers/company_dashboard_controller.rb
+++ app/controllers/company_dashboard_controller.rb
@@ -3,7 +3,20 @@ class CompanyDashboardController < ApplicationController
 		@user = User.first
 	end
 
+	def job_post
+		@job_posts = Jobpost.where(company_id:session[:id])
+	end
 	def potential_candidates
-		@matches = User.all
+		job_id = session[:jobid]
+		current_job = Jobpost.where(id: job_id).first
+		location = current_job.job_location
+		job_type = current_job.job_type
+		list_of_users = User.all
+		@matches = []
+		list_of_users.each do |user|
+			if user.preferred_location.split("/").include?(location) and user.preferred_jobs.split("/").include?(job_type)
+				@matches<<user
+			end
+		end
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Microjobs::Application.routes.draw do
 
   get "/user" => "CompanyDashboard#user_profile"
   get "/candidate_matches" => "CompanyDashboard#potential_candidates"
-  
+  get "/job_post" =>"CompanyDashboard#job_post"
+
   get "/job_matches" => "User#potential_jobs"
   get "/job" =>"User#job"
   
