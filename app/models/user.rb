9722c77c75112afce821897a697995b0dc30e6f0
prasanna <praserocking@gmail.com>
2015-08-16 03:38:22 +0530

    added merging contact and list

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
-  attr_accessible :available, :experience, :mob_num, :name, :password_digest, :preferred_jobs, :preferred_location, :ratings, :user_type, :password_confirmation, :password
+  attr_accessible :available, :experience, :mob_num, :name, :password_digest, :preferred_jobs, :preferred_location, :ratings, :user_type, :password_confirmation, :password_confirmation
   has_secure_password
 end
