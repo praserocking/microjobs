817581d20f2a818be53925ea6a1adcd22b1fa62e
prasanna <praserocking@gmail.com>
2015-08-15 13:47:56 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microjobs::Application.config.secret_token = 'bc2133346ae800d7acf3d36db1d33787a113bbb56da0c9bad6098c3359a2ba263fb84735bff0ff5070d06fa1e592b6fe8878a60cddeff5337eca7150491af0e4'
