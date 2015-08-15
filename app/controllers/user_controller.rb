3078e622621106c52658e3f9f9db2f5d5dcd8bf0
sivag123 <sivag123@gmail.com>
2015-08-15 23:47:34 +0530

    Agent,comp signups

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,7 +5,13 @@ class UserController < ApplicationController
 	end
 
 	def signup
-		@user = User.new(name: params[:name], mob_num: params[:mob_num], preferred_jobs: params[:preferred_jobs].join('/'), preferred_location: params[:preferred_location].join('/'),ratings: 0.0,available: true, experience: 0,password: params[:password])		
+		if params[:type]=="user"
+			@user = User.new(name: params[:name], mob_num: params[:mob_num], preferred_jobs: params[:preferred_jobs].join('/'), preferred_location: params[:preferred_location].join('/'),ratings: 0.0,available: true, experience: 0,password: params[:password])			
+		elsif params[:type]=="agent"
+			@user = Agent.new(name: params[:name], mob_num: params[:mob_num], max_emp_count: params[:max_emp_count], preferred_emp_type: params[:preferred_emp_type].join('/'),preferred_job_location: params[:preferred_job_location].join('/'),ratings: 0.0,available: true, experience: 0,password: params[:password])		
+		else
+			@user = Company.new(name: params[:name], organisation: params[:organisation], organisation_type: params[:organisation_type].join('/'), email: params[:email],address: params[:address],password: params[:password])		
+		end
 		if @user.save
 			puts "Success"
 		end
